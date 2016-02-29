f353a1c68044c5965126589d6ed886d58d691101
faiz636 <muhammad_faiz@live.com>
2016-02-29 09:49:41 +0500

    sending notification messages to gcm at topic with collaps key and priority

--- gcmTest1/test/demo_superagent/notificationHttpRequest.js
+++ gcmTest1/test/demo_superagent/notificationHttpRequest.js
@@ -5,25 +5,42 @@ var superagent = require("superagent");
 var address;
 address = 'https://gcm-http.googleapis.com/gcm/send';
 //address = 'localhost:4000/test';
-superagent
-//.get('localhost:4000/test')
-    .post(address)
-    .set('Authorization', 'key=AIzaSyCHweEU0gXdGxQdFDIvWWh4bjbiIIck3m8')
+var i = 0;
+for (i = 0; i < 50; i++) {
+    superagent
+    //.get('localhost:4000/test')
+        .post(address)
+        .set('Authorization', 'key=AIzaSyCHweEU0gXdGxQdFDIvWWh4bjbiIIck3m8')
 
-    .send(
-        {
-            "to" : "cULPqFEQfK0:APA91bFq7EQhY78v6UM6eEgMD_szPGQH7E6-c7Ve6htoyxlT574LXqLeY9ja8mb1p-ogzhy3ws8HDGatximnflcRPs0fhggJAzNa1MNz597F9Wv1MJrBfPvgzmA22fqKmK27GxQw3qRO",
-            "notification" : {
-                "body" : "great match!",
-                "title" : "Portugal vs. Denmark",
-                "icon" : "myicon"
+        .send(
+            {
+                "collapse_key" : "demo",
+                "priority" : "high",
+                "to" : "/topics/global",
+                //"to" : "e2FaQFC4Y2c:APA91bFqj6cD9yhQTPrrDGL6ZVUjRRkWZiyAS_T6eFnoU2QKQXQacPpVr1n7PR_kMgnu6mpYnmAyIfILzthOm_Nw41OQTVdWpAbhDFD1nbIyFp3AifCnxr99eeosp6IiQxMj5-zVX-y_",
+                //"to" : "eE1k3c3oTn8:APA91bHp91A5wtvJThHatU_Atn7jR4UZ4c84uw4DJWLagufCYLuOSJzRTlbpwZgZkT0URMo9I5bmYpV3pyF3roaaByDZMxIurP17SgUq-XCQ5jDR0IiqSFh_dyp5MOo3VDthRRS0ZWKc",
+/*
+                "registration_ids": [
+                    "fY_FWe4u_TA:APA91bGIdtLFJXdCTDJkTwsozinW3sj_AWrPp7mT2RlhfRledpIvl3I5bCsE7Hvu_kfcsYK6V6VQ2hD75mLTIvD9VJ-v0C9cB_dCg_yrgLXQlkC7T-cjY7UxA4ehRlnGOYYV-zbYnVhY",
+                    "coy6lZt5c4s:APA91bGGf-lXaesgPYhsNVlcJ_hmTMbrJdTNoQlCqtMd-XiWxv83uSr8o2SApgt6XbCo_J8jQtptMuDbiMRuuBmSmh03yyf29MMPmlG0FlivgNMDDfXhL-7CTEfpfqJIvR2Zih4RpDo1"
+                ],
+*/
+                "notification": {
+                    "body": "testing is good",
+                    "title": "good test title" + (100 + i),
+                    "icon": "myicon"
+                }
             }
-        }
-    )
+        )
 
-    .end(function (err, res) {
-        console.log("response");
-        //console.log(err);
-        //console.log(res);
+        .end(function (err, res) {
+            console.log("response");
+            //if (err != null) {
+                console.log(err);
+            //}
+            console.log("================================");
+            console.log(res.text);
+            console.log(res.body);
 
-    });
+        });
+}
